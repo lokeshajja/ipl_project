af4d4693b3e393edf3d0890fa1ad2890c9bebe7c
Lokesh <lokesh.2@mountblue.tech>
2019-12-03 20:21:16 +0530

    test economic bowlers pylint errors checked

--- ipl/test_economic_bowlers.py
+++ ipl/test_economic_bowlers.py
@@ -3,43 +3,37 @@ from file_extraction import extract_file
 from most_econ_bowlers_2015 import economy_of_bowlers
 
 
-
-
-
-mock_data=extract_file('matches.csv')
-count=0
-mock_matches=[]
+mock_data = extract_file('matches.csv')
+count = 0
+mock_matches = []
 for mockdata in mock_data:
-    if count<15:
+    if count < 15:
         mock_matches.append(mockdata)
-        count+=1
+        count += 1
 
 
-delivery_data=extract_file('deliveries.csv')
-serial=0
-mock_deliveries=[]
+delivery_data = extract_file('deliveries.csv')
+serial = 0
+mock_deliveries = []
 for ball_data in delivery_data:
-    if serial<15:
+    if serial < 15:
         mock_deliveries.append(ball_data)
-        serial+=1
-
-
-
+        serial += 1
 
 
 class TestPlotData(unittest.TestCase):
 
     def test_economy_of_bowlers(self):
 
-        bowler_data={'UT Yadav': {'balls': 60, 'runs': 40}, 'M Morkel': {'balls': 120, 'runs': 100}, 'Shakib Al Hasan': {'balls': 60, 'runs': 50}}
-        economy_data={'UT Yadav': 4, 'M Morkel': 5, 'Shakib Al Hasan': 5 }
-        inputs_and_outputs=[(bowler_data,economy_data)]
-        for inputs,expected_output in inputs_and_outputs:
+        bowler_data = {'UT Yadav': {'balls': 60, 'runs': 40}, 'M Morkel':
+                       {'balls': 120, 'runs': 100}, 'Shakib Al Hasan':
+                       {'balls': 60, 'runs': 50}}
+        economy_data = {'UT Yadav': 4, 'M Morkel': 5,
+                        'Shakib Al Hasan': 5}
+        inputs_and_outputs = [(bowler_data, economy_data)]
+        for inputs, expected_output in inputs_and_outputs:
             output = economy_of_bowlers(inputs)
             self.assertEqual(output, expected_output)
-
-
-
 
 
 if __name__ == '__main__':

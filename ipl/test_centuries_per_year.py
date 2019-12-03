bf0b66d236993732db5b68dd7b11be634554a6de
Lokesh <lokesh.2@mountblue.tech>
2019-12-03 11:36:04 +0530

    centuries per year test case rechecked

--- ipl/test_centuries_per_year.py
+++ ipl/test_centuries_per_year.py
@@ -24,14 +24,22 @@ for delivery in delivery_data:
         serial+=1
 
 
+
+
+
 class TestPlotData(unittest.TestCase):
+
     def test_ids_of_each_year(self):
+
         yr_data={ '2017' : {} }
         op={ '2017' : { '1' : {} , '2' : { }, '3' : {} , '4' : {} , '5' : { }} }
         inputs_and_outputs=[((yr_data,mock_matches),op)]
-        for input1,output1 in inputs_and_outputs:
-            output = ids_of_each_year(input1[0],input1[1])
-            self.assertEqual(output, output1)
+        for inputs,expected_output in inputs_and_outputs:
+            output = ids_of_each_year(inputs[0],inputs[1])
+            self.assertEqual(output, expected_output)
+
+
+
 
 
 if __name__ == '__main__':

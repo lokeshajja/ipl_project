ef9a0ba1fd57ad2f130f00de31517ed7835d9434
Lokesh <lokesh.2@mountblue.tech>
2019-12-03 15:47:25 +0530

    matches played per year flake8 errors and feedback errors sorted

--- ipl/matches_played_per_year.py
+++ ipl/matches_played_per_year.py
@@ -1,45 +1,31 @@
-import os
-import sys
-import numpy as np
 from matplotlib import pyplot as plt
+from collections import OrderedDict
 
 
+def plot_number_of_matches_per_year(matches_per_year):
 
-
-
-def plot_the_number_of_matches_per_year(matches_per_year):
-
-    plot_data=matches_per_year
-    years=list(plot_data.keys())
-    matches=list(plot_data.values())
-    plt.bar(years,matches)
+    plot_data = matches_per_year
+    years = list(plot_data.keys())
+    matches = list(plot_data.values())
+    plt.bar(years, matches)
+    plt.title('Matches Played Per Year')
     plt.show()
 
 
-
-
-
-def compute_number_of_matches_yearwise(matches):
-   
-    year_wise_match_counts={}
+def compute_number_of_matches_per_year(matches):
+    match_per_year = OrderedDict()
     for match in matches:
-        year_wise_match_counts[match['season']] = year_wise_match_counts.get(match['season'],0)+1
-    
-    
-    print(year_wise_match_counts)
-    year_wise_matches={}
-    for year in sorted(year_wise_match_counts.keys()):
-        year_wise_matches[year]=year_wise_match_counts[year]
-
-
-    return year_wise_matches
-
-
-
+        match_per_year[match['season']] = \
+            match_per_year.get(match['season'], 0) + 1
+    matches_per_year = {}
+    for year in sorted(match_per_year.keys()):
+        matches_per_year[year] = match_per_year[year]
+    print('matches played per year are ')
+    print(matches_per_year)
+    return matches_per_year
 
 
 def compute_and_plot_matches_played_per_year(matches):
 
-    matches_per_year=compute_number_of_matches_yearwise(matches)
-    plot_the_number_of_matches_per_year(matches_per_year)
-    
+    matches_per_year = compute_number_of_matches_per_year(matches)
+    plot_number_of_matches_per_year(matches_per_year)

6acdf53ca51a35dadb0cefdc8f62f94f2ef4977a
salokesh <salokesh99@gmail.com>
2019-12-04 18:47:42 +0530

    rechecked according to the feedback fully modified

--- ipl/test_extras_of_2016.py
+++ ipl/test_extras_of_2016.py
@@ -3,33 +3,20 @@ from file_extraction import extract_data
 from extras_of_year_2016 import compute_extra_runs
 
 
-mock_data = extract_data('matches.csv')
-count = 0
-mock_matches = []
-for mockdata in mock_data:
-    if count < 15:
-        mock_matches.append(mockdata)
-        count += 1
+delivery_data = extract_data('mock_deliveries_data.csv')
 
 
-delivery_data = extract_data('deliveries.csv')
-counts = 0
-mock_deliveries = []
-for ball_data in delivery_data:
-    if counts < 15:
-        mock_deliveries.append(ball_data)
-        counts += 1
-
-
-class TestPlotData(unittest.TestCase):
+class TestExtraRunsConcededPerTeam2016(unittest.TestCase):
 
     def test_id_wise_data(self):
-
         inputs_and_outputs =\
-         [((['1'], mock_deliveries), {'Royal Challengers Bangalore': 4})]
-        for inputs, outputs in inputs_and_outputs:
+         [((['594', '607', '631', '632', '633', '634', '635'], delivery_data),
+             {'Sunrisers Hyderabad': 2, 'Gujarat Lions': 1,
+              'Kolkata Knight Riders': 2, 'Delhi Daredevils': 5,
+              'Royal Challengers Bangalore': 1})]
+        for inputs, expected_output in inputs_and_outputs:
             output = compute_extra_runs(inputs[0], inputs[1])
-            self.assertEqual(output, outputs)
+            self.assertEqual(output, expected_output)
 
 
 if __name__ == '__main__':

6d1a8aed6e761ac12f4d99e2f56ca97be951ec0f
salokesh <salokesh99@gmail.com>
2019-12-04 13:01:25 +0530

    used with open to open a file

--- ipl/file_extraction.py
+++ ipl/file_extraction.py
@@ -3,8 +3,8 @@ import csv
 
 def extract_file_data(file_name):
 
-    file = open(file_name, 'r')
-    raw_data = csv.DictReader(file)
-    data_array = list(raw_data)
+    with open(file_name, 'r') as file:
+        raw_data = csv.DictReader(file)
+        data_array = list(raw_data)
 
     return data_array

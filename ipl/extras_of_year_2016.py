319e0fa766d64b7d543c07e8d7a514e0a598b9c1
salokesh <salokesh99@gmail.com>
2019-12-04 14:54:15 +0530

    extras of year 2016 functions name rechecked

--- ipl/extras_of_year_2016.py
+++ ipl/extras_of_year_2016.py
@@ -1,8 +1,7 @@
 from matplotlib import pyplot as plt
 
 
-def year_and_id_data(matches):
-    season = '2016'
+def year_and_id_data(matches, season):
     matches_of_2016 = []
     for match in matches:
         if match['season'] == season:
@@ -11,7 +10,7 @@ def year_and_id_data(matches):
     return matches_of_2016
 
 
-def years_and_ids(matches_of_2016, deliveries_data):
+def compute_extra_runs(matches_of_2016, deliveries_data):
 
     ids = matches_of_2016
     teams_and_extra_runs = {}
@@ -26,18 +25,10 @@ def years_and_ids(matches_of_2016, deliveries_data):
     return teams_and_extra_runs
 
 
-def extra_runs_conceived_by_each_team_in_2016(matches, deliveries_data):
+def plot_extra_runs_given_by_each_team(extra_runs_data):
 
-    matches_of_2016 = year_and_id_data(matches)
-    extra_runs_data = years_and_ids(matches_of_2016, deliveries_data)
-
-    return extra_runs_data
-
-
-def plot_of_extra_runs_given_by_each_team_in_2016(plot_data):
-
-    x = plot_data.keys()
-    y = plot_data.values()
+    x = extra_runs_data.keys()
+    y = extra_runs_data.values()
     plt.bar(x, y)
     plt.title('Extra Runs Given By Each Team In 2016')
     plt.xlabel('Teams')
@@ -45,3 +36,10 @@ def plot_of_extra_runs_given_by_each_team_in_2016(plot_data):
     plt.setp(plt.gca().get_xticklabels(), rotation=30,
              horizontalalignment='right')
     plt.show()
+
+
+def compute_and_plot_extra_runs_given_by_each_team(matches, deliveries_data):
+    season = '2016'
+    matches_of_2016 = year_and_id_data(matches, season)
+    extra_runs_data = compute_extra_runs(matches_of_2016, deliveries_data)
+    plot_extra_runs_given_by_each_team(extra_runs_data)

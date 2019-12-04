3b74d096c9a1910a52b1b2affe6c559dc5ab1386
salokesh <salokesh99@gmail.com>
2019-12-04 13:18:29 +0530

    file extraction with open used

--- ipl/file_extraction.py
+++ ipl/file_extraction.py
@@ -1,10 +1,10 @@
 import csv
 
 
-def extract_file_data(file_name):
+def extract_data(file_name):
 
     with open(file_name, 'r') as file:
         raw_data = csv.DictReader(file)
-        data_array = list(raw_data)
+        data_read = list(raw_data)
 
-    return data_array
+    return data_read

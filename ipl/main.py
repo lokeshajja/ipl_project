24dcade0aa464a5077c2dd7f44926c92b701b318
salokesh <salokesh99@gmail.com>
2019-12-04 14:53:13 +0530

    extras of year 2016 imports cancelled

--- ipl/main.py
+++ ipl/main.py
@@ -2,8 +2,8 @@ from matches_played_per_year import \
     compute_and_plot_number_of_matches_played_per_year
 from number_of_matches_won_by_each import \
     compute_and_plot_matches_won_by_each_team_over_all_seasons
-from extras_of_year_2016 import extra_runs_conceived_by_each_team_in_2016
-from extras_of_year_2016 import plot_of_extra_runs_given_by_each_team_in_2016
+from extras_of_year_2016 import \
+    compute_and_plot_extra_runs_given_by_each_team
 from most_econ_bowlers_2015 import\
     compute_and_plot_most_economic_bowlers_of_2015
 from max_centuries import compute_centuries_data_over_all_years
@@ -18,9 +18,7 @@ def main():
 
     compute_and_plot_matches_won_by_each_team_over_all_seasons(matches)
 
-    extras_given_by_each_team = \
-        extra_runs_conceived_by_each_team_in_2016(matches, deliveries)
-    plot_of_extra_runs_given_by_each_team_in_2016(extras_given_by_each_team)
+    compute_and_plot_extra_runs_given_by_each_team(matches, deliveries)
 
     compute_and_plot_most_economic_bowlers_of_2015(matches, deliveries)
 
@@ -29,3 +27,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    

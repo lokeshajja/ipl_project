76170f2c8ac26131e15532dcdd707e401374056c
salokesh <salokesh99@gmail.com>
2019-12-04 15:50:48 +0530

    tect matches per season renamed

--- ipl/test_matches_per_season.py
+++ ipl/test_matches_per_season.py
@@ -3,17 +3,14 @@ from file_extraction import extract_data
 from matches_played_per_year import compute_number_of_matches_played_per_year
 
 mock_data = extract_data('matches.csv')
-count = 0
 mock_matches = []
-for mockdata in mock_data:
-    if count < 69:
-        mock_matches.append(mockdata)
-        count += 1
+for mockdata in mock_data[:69]:
+    mock_matches.append(mockdata)
 
 
 class TestMatchesPerSeason(unittest.TestCase):
 
-    def test_compute_number_of_matches_yearwise1(self):
+    def test_compute_number_of_matches_yearwise(self):
 
         inputs_and_outputs = [(mock_matches, {'2017': 59, '2008': 10})]
         for inputs, expected_output in inputs_and_outputs:

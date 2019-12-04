3da24e886cc0a53ca92d4feb0ba0909ac49419fa
salokesh <salokesh99@gmail.com>
2019-12-04 14:52:33 +0530

    most economic bowlers  2015 rechecked for names

--- ipl/most_econ_bowlers_2015.py
+++ ipl/most_econ_bowlers_2015.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
+from extras_of_year_2016 import year_and_id_data
 
 
-def economy_of_bowlers(bowler_data):
+def calculate_economy_of_bowlers(bowler_data):
 
     economy_data = {}
     for bowler, bowler_data in bowler_data.items():
@@ -36,7 +37,7 @@ def plot_top_economy_data(ordered_economy):
     plt.show()
 
 
-def compute_matches_of_2015(ids_of_2015, deliveries_data):
+def compute_economy_of_bowlers(ids_of_2015, deliveries_data):
 
     bowler_data = {}
     for ball in deliveries_data:
@@ -45,31 +46,23 @@ def compute_matches_of_2015(ids_of_2015, deliveries_data):
                 bowler_data[ball['bowler']] = {}
                 bowler_data[ball['bowler']]['balls'] = 1
                 bowler_data[ball['bowler']]['runs'] = int(ball['total_runs'])
-            elif ball['bowler'] in bowler_data:
+            else:
                 bowler_data[ball['bowler']]['balls'] += 1
                 bowler_data[ball['bowler']]['runs'] += int(ball['total_runs'])
-            else:
-                pass
 
-    economy_statistics = economy_of_bowlers(bowler_data)
+    economy_statistics = calculate_economy_of_bowlers(bowler_data)
     ordered_economy = []
 
     for economy in sorted(economy_statistics, key=economy_statistics.get):
         bowler_economy = (economy, economy_statistics[economy])
         ordered_economy.append(bowler_economy)
 
-    plot_top_economy_data(ordered_economy)
-
-
-def matches_played_in_2015(matches):
-    ids_of_2015 = []
-    for match_data in matches:
-        if match_data['season'] == '2015':
-            ids_of_2015.append(match_data['id'])
-
-    return ids_of_2015
+    return ordered_economy
 
 
 def compute_and_plot_most_economic_bowlers_of_2015(matches, deliveries_data):
-    ids_of_2015 = matches_played_in_2015(matches)
-    compute_matches_of_2015(ids_of_2015, deliveries_data)
+    season = '2015'
+    ids_of_2015 = year_and_id_data(matches, season)
+    economy_of_bowlers = compute_economy_of_bowlers(ids_of_2015,
+                                                    deliveries_data)
+    plot_top_economy_data(economy_of_bowlers)

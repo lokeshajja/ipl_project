79d1ae8fe271c197a4d351a698526597119e8ceb
salokesh <salokesh99@gmail.com>
2019-12-06 19:37:51 +0530

    main created with 2 functions

--- ipl/sql_main.py
+++ ipl/sql_main.py
@@ -1,10 +1,14 @@
 from sql_matches_played_per_year import \
-    compute_number_of_matches_per_year
+    compute_and_plot_number_of_matches_per_year
+from sql_number_of_matches_won import \
+    compute_and_plot_number_of_matches_per_season_of_all_teams
 
 
 def main():
 
-    compute_number_of_matches_per_year()
+    compute_and_plot_number_of_matches_per_year()
+
+    compute_and_plot_number_of_matches_per_season_of_all_teams()
 
 
 if __name__ == "__main__":

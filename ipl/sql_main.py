b67b388e65104ec4057806f98fe25cd0c60a5783
salokesh <salokesh99@gmail.com>
2019-12-06 18:36:59 +0530

    sql main created

---
+++ ipl/sql_main.py
@@ -0,0 +1,11 @@
+from sql_matches_played_per_year import \
+    compute_number_of_matches_per_year
+
+
+def main():
+
+    compute_number_of_matches_per_year()
+
+
+if __name__ == "__main__":
+    main()

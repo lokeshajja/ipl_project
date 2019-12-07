4159977c3d2c329e5e5a7cb845b42c9dfd37f674
salokesh <salokesh99@gmail.com>
2019-12-07 22:07:28 +0530

    extras of 2015 updated

--- ipl/sql_main.py
+++ ipl/sql_main.py
@@ -2,6 +2,7 @@ from sql_matches_played_per_year import \
     compute_and_plot_number_of_matches_per_year
 from sql_number_of_matches_won import \
     compute_and_plot_number_of_matches_per_season_of_all_teams
+from sql_extras_of_2016 import compue_and_plot_extras_of_2016
 
 
 def main():
@@ -9,6 +10,8 @@ def main():
     compute_and_plot_number_of_matches_per_year()
 
     compute_and_plot_number_of_matches_per_season_of_all_teams()
+
+    compue_and_plot_extras_of_2016()
 
 
 if __name__ == "__main__":

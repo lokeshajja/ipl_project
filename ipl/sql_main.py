5a2488377f6599728833948f8f53adf64347a202
salokesh <salokesh99@gmail.com>
2019-12-07 22:24:07 +0530

    added sql most economic bowlers of 2015

--- ipl/sql_main.py
+++ ipl/sql_main.py
@@ -3,6 +3,8 @@ from sql_matches_played_per_year import \
 from sql_number_of_matches_won import \
     compute_and_plot_number_of_matches_per_season_of_all_teams
 from sql_extras_of_2016 import compue_and_plot_extras_of_2016
+from sql_most_economic_bowlers_of_2015 import \
+    sql_most_economic_bowlers_2015
 
 
 def main():
@@ -12,6 +14,8 @@ def main():
     compute_and_plot_number_of_matches_per_season_of_all_teams()
 
     compue_and_plot_extras_of_2016()
+
+    sql_most_economic_bowlers_2015()
 
 
 if __name__ == "__main__":
